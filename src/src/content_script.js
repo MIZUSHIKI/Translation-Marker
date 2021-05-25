449bba61879c83b8a330e99dc8cd28a3331f19ff
MIZUSHIKI <33353602+MIZUSHIKI@users.noreply.github.com>
2021-05-26 04:08:09 +0900

    Add files via upload
    ・マーカー幅の設定値がそのままの%になるようCSS側を「(100-設定値)%」に修正。
    ・オプションなどのテキストをいくつか見直し。
    ・その他、細かい箇所の変更と修正。

---
+++ src/src/content_script.js
@@ -0,0 +1,41 @@
+﻿
+window.addEventListener("load", init, false);
+
+function init() {
+	chrome.storage.sync.get({ onlyCheck:false, onlySites:'https://www.youtube.com/*\nhttps://twitter.com/*' }, function (items) {
+	
+		if( !items.onlyCheck ){
+			setMarkerStyle()
+    		return;
+		}
+	
+		var arr = items.onlySites.split(/\r\n|\n/);
+		if( items.onlySites == '' ){
+			arr = ['https://www.youtube.com/*','https://twitter.com/*'];
+		}
+		
+		for (var i = 0; i < arr.length; i++){
+    		if( matchRuleShort(location.href,arr[i]) ){
+				setMarkerStyle()
+    			return;
+    		}
+		}
+		
+	});
+}
+function setMarkerStyle(){
+	const style = document.createElement('style');
+	chrome.storage.sync.get({	height:60,
+								color:'#ffff60',
+								where:'font[style="vertical-align: inherit;"]'
+								}, function (items) {
+	style.innerHTML = `${items.where}{background:linear-gradient(transparent ${items.height}%, ${items.color} ${items.height}%);}`;
+		document.body.appendChild(style);
+	});
+}
+
+//Wildcard comparison
+function matchRuleShort(str, rule) {
+  var escapeRegex = (str) => str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  return new RegExp("^" + rule.split("*").map(escapeRegex).join(".*") + "$").test(str);
+}
